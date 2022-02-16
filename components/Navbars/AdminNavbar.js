ecebb032d5742fba100e397f0a375d0a003679eb
SAMBIT MAJHI <majhisambit2@gmail.com>
2022-02-17 01:26:12 +0530

    Update AdminNavbar.js

--- components/Navbars/AdminNavbar.js
+++ components/Navbars/AdminNavbar.js
@@ -8,12 +8,12 @@ export default function Navbar() {
   return (
     <>
       {/* Navbar */}
-      <nav className="absolute top-0 left-0 w-full z-10 bg-blueGray-700 md:flex-row md:flex-nowrap md:justify-start flex items-center p-4">
+      <nav className="absolute top-0 left-0 w-full z-10 bg-transparent md:flex-row md:flex-nowrap md:justify-start flex items-center p-4">
         <div className="w-full mx-autp items-center flex justify-between md:flex-nowrap flex-wrap md:px-10 px-4">
           {/* Brand */}
           <a
             className="text-white text-sm uppercase hidden lg:inline-block font-semibold"
-            href="#pablo"
+            href="#"
             onClick={(e) => e.preventDefault()}
           >
             {router.pathname.split("/").at(-1)}
